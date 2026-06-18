19e4ca2dd7747b494af6a286b028d77bcc8bf789
xstrem39 <xstrem39@gmail.com>
2026-06-18 17:08:45 +0000

    Update validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -6,8 +6,6 @@ export const validateEmail = (email) => {
 export const validatePassword = (password) => {
   const errors = [];
   if (!password || password.length < 8) errors.push('Password must be at least 8 characters');
-  if (!/[A-Z]/.test(password)) errors.push('Must contain at least one uppercase letter');
-  if (!/[0-9]/.test(password)) errors.push('Must contain at least one number');
   return { valid: errors.length === 0, errors };
 };
 
